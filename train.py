c73fcb471d44552dbd6b9ada0abe54e5482edd76
mb715 <mb715@sussex.ac.uk>
2019-03-18 16:28:26 +0000

    Correct arguments in discriminator forward pass.

--- train.py
+++ train.py
@@ -142,7 +142,7 @@ def compute_loss(x, s, model, discriminator, *, return_z=False):
     # mmd = metrics.MMDStatistic(z_s0.size(0), z_s1.size(0))
     # mmd_loss = mmd(z_s0[:, :-ARGS.zs_dim], z_s1[:, :-ARGS.zs_dim], alphas=[1])
     # mmd_loss *= ARGS.independence_weight
-    mmd_loss = F.binary_cross_entropy(discriminator(z_s0), s)
+    mmd_loss = F.binary_cross_entropy(discriminator(z[:, :-ARGS.zs_dim]), s)
     # disc_loss_function =
 
     log_pz = utils.standard_normal_logprob(z).view(z.shape[0], -1).sum(1, keepdim=True)  # logp(z)
