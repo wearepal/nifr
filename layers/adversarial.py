81e93efb03a1f00ab122ea9f8c881ad44a68b985
mb715 <mb715@sussex.ac.uk>
2019-03-18 15:48:41 +0000

    Added mlp network and gradient reversal to layers

---
+++ layers/adversarial.py
@@ -0,0 +1,30 @@
+import torch.nn as nn
+from torch.autograd import Function
+from layers.mlp import Mlp
+
+
+class GradReverse(Function):
+    """Gradient reversal layer"""
+
+    @staticmethod
+    def forward(ctx, x):
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        return grad_output.neg()
+
+
+def _grad_reverse(features):
+    return GradReverse.apply(features)
+
+
+class GradReverseDiscriminator(nn.Module):
+
+    def __init__(self, mlp_sizes, activation=nn.ReLU, activation_out=nn.Sigmoid):
+        super(GradReverseDiscriminator, self).__init__()
+        self.mlp = Mlp(mlp_sizes, activation, activation_out)
+
+    def forward(self, x):
+        x = _grad_reverse(x)
+        return self.mlp(x)
