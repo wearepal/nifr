1198bb9b2f7de880dd0ce5f98d1c598d78f8ce39
Oliver Thomas <ot44@sussex.ac.uk>
2019-03-19 16:08:57 +0000

    added weight_adjustment model

--- run.py
+++ run.py
@@ -15,6 +15,7 @@ from ethicml.preprocessing.train_test_split import train_test_split
 from ethicml.metrics import Accuracy, ProbPos
 
 from train import main as training_loop
+from weight_adjust import main as weight_adjustment
 
 
 # class ModelWrapper(BasicTPA):
@@ -38,6 +39,7 @@ def main():
 
     experiment = Experiment(api_key="Mf1iuvHn2IxBGWnBYbnOqG23h",
                             project_name="finn", workspace="olliethomas")
+    experiment.disable_mp()
 
     data = load_data(Adult())
     train, test = train_test_split(data)
@@ -45,15 +47,25 @@ def main():
 
     lr = LR()
 
-    print("Original:")
-    train_unfair = DataTuple(x=pd.concat([train.x, train.s], axis='columns'), s=train.s, y=train.y)
-    test_unfair = DataTuple(x=pd.concat([test.x, test.s], axis='columns'), s=test.s, y=test.y)
-    preds_unfair = lr.run(train_unfair, test_unfair)
-    results = run_metrics(preds_unfair, test_unfair, [Accuracy()], [ProbPos()])
+    print("Original x & s:")
+    train_x_and_s = DataTuple(x=pd.concat([train.x, train.s], axis='columns'), s=train.s, y=train.y)
+    test_x_and_s = DataTuple(x=pd.concat([test.x, test.s], axis='columns'), s=test.s, y=test.y)
+    preds_x_and_s = lr.run(train_x_and_s, test_x_and_s)
+    results = run_metrics(preds_x_and_s, test_x_and_s, [Accuracy()], [ProbPos()])
     experiment.log_metric("Original Accuracy", results['Accuracy'])
     experiment.log_metric("Original P(Y=1|s=0)", results['sex_Male_0_prob_pos'])
     experiment.log_metric("Original P(Y=1|s=1)", results['sex_Male_1_prob_pos'])
-    print(results)
+    print(results, "\n")
+
+    print("All z:")
+    train_z = DataTuple(x=train_all, s=train.s, y=train.y)
+    test_z = DataTuple(x=test_all, s=test.s, y=test.y)
+    preds_z = lr.run(train_z, test_z)
+    results = run_metrics(preds_z, test_z, [Accuracy()], [ProbPos()])
+    experiment.log_metric("Z Accuracy", results['Accuracy'])
+    experiment.log_metric("Z P(Y=1|s=0)", results['sex_Male_0_prob_pos'])
+    experiment.log_metric("Z P(Y=1|s=1)", results['sex_Male_1_prob_pos'])
+    print(results, "\n")
 
     print("fair:")
     train_fair = DataTuple(x=train_zx, s=train.s, y=train.y)
@@ -63,7 +75,17 @@ def main():
     experiment.log_metric("Fair Accuracy", results['Accuracy'])
     experiment.log_metric("Fair P(Y=1|s=0)", results['sex_Male_0_prob_pos'])
     experiment.log_metric("Fair P(Y=1|s=1)", results['sex_Male_1_prob_pos'])
-    print(results)
+    print(results, "\n")
+
+    print("unfair:")
+    train_unfair = DataTuple(x=train_zs, s=train.s, y=train.y)
+    test_unfair = DataTuple(x=test_zs, s=test.s, y=test.y)
+    preds_unfair = lr.run(train_unfair, test_unfair)
+    results = run_metrics(preds_unfair, test_unfair, [Accuracy()], [ProbPos()])
+    experiment.log_metric("Unfair Accuracy", results['Accuracy'])
+    experiment.log_metric("unfair P(Y=1|s=0)", results['sex_Male_0_prob_pos'])
+    experiment.log_metric("Unfair P(Y=1|s=1)", results['sex_Male_1_prob_pos'])
+    print(results, "\n")
 
     print("predict s from fair representation:")
     train_fair_predict_s = DataTuple(x=train_zx, s=train.s, y=train.s)
@@ -80,6 +102,10 @@ def main():
     results = run_metrics(preds_s_unfair, test_unfair_predict_s, [Accuracy()], [])
     experiment.log_metric("Unfair pred s", results['Accuracy'])
     print(results)
+
+    weight_adjustment(DataTuple(x=train_all, s=train.s, y=train.y),
+                      DataTuple(x=test_all, s=test.s, y=test.y),
+                      train_zs.shape[1])
 
 
 if __name__ == "__main__":
---
+++ weight_adjust.py
@@ -0,0 +1,74 @@
+import copy
+
+import torch
+from torch import nn
+from tqdm import tqdm
+import torch.nn.functional as F
+
+import pandas as pd
+
+from src.ethicml.ethicml.algorithms.dataloader_funcs import CustomDataset
+from src.ethicml.ethicml.evaluators.per_sensitive_attribute import metric_per_sensitive_attribute, \
+    diff_per_sensitive_attribute, ratio_per_sensitive_attribute
+from src.ethicml.ethicml.metrics import Accuracy, ProbPos
+from src.ethicml.ethicml.utility.heaviside import Heaviside
+
+
+class Model(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.layer = nn.Linear(input_size, 1)
+        self.act = nn.Sigmoid()
+
+    def forward(self, x):
+        return self.act(self.layer(x))
+
+
+def main(train=None, test=None, S_SECTION=None):
+    train_data = CustomDataset(train)
+    train_loader = torch.utils.data.DataLoader(dataset=train_data, batch_size=64, shuffle=False)
+
+    test_data = CustomDataset(test)
+    test_loader = torch.utils.data.DataLoader(dataset=test_data, batch_size=64, shuffle=False)
+
+    model = Model(train.x.shape[1])
+    optimizer_y = torch.optim.Adam(model.parameters())
+    for i in tqdm(range(75)):
+        for b, (embedding, sens_label, class_label) in enumerate(train_loader):
+            y_pred = model(embedding)
+
+            y_loss = F.binary_cross_entropy(y_pred, class_label)
+
+            optimizer_y.zero_grad()
+            y_loss.backward()
+            optimizer_y.step()
+        optimizer_y.zero_grad()
+
+    model.eval()
+    preds = []
+    for embedding, sens_label, class_label in test_loader:
+        preds += model(embedding).data.numpy().tolist()
+
+    heavi = Heaviside()
+
+    untouched_weights = copy.deepcopy(model.layer)
+    for mul in [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]:
+        model.layer.weight[:, -S_SECTION:] = untouched_weights.weight[:, -S_SECTION:] * mul
+
+        preds = []
+        for embedding, sens_label, class_label in test_loader:
+            preds += model(embedding).data.numpy().tolist()
+
+        preds = pd.DataFrame(preds, columns=['preds'])
+        preds = pd.DataFrame(heavi.apply(preds.values), columns=['preds'])
+        per_sens_dict = metric_per_sensitive_attribute(preds, test, ProbPos())
+        diff = diff_per_sensitive_attribute(per_sens_dict)
+        ratio = ratio_per_sensitive_attribute(per_sens_dict)
+        print(f"{mul:3.1f} z(n-{S_SECTION}) -> y {per_sens_dict} \t "
+              f"ratio: {list(ratio.values())[0]:.5f} \t "
+              f"diff: {list(diff.values())[0]:.5f} \t "
+              f"Acc: {Accuracy().score(preds['preds'], test):.5f}")
+
+
+if __name__ == '__main':
+    main()
