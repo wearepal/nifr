531f7450730df48a4edefb5c933d84873d00e472
Oliver Thomas <ot44@sussex.ac.uk>
2019-03-19 13:38:21 +0000

    comet ml set up to record various experiments

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import sys
 from pathlib import Path
 
 import pandas as pd
+from comet_ml import Experiment
 
 from ethicml.algorithms.preprocess.threaded.threaded_pre_algorithm import BasicTPA
 from ethicml.algorithms.inprocess.logistic_regression import LR
@@ -34,17 +35,24 @@ class ModelWrapper(BasicTPA):
 
 def main():
     # model = ModelWrapper()
+
+    experiment = Experiment(api_key="Mf1iuvHn2IxBGWnBYbnOqG23h",
+                            project_name="finn", workspace="olliethomas")
+
     data = load_data(Adult())
     train, test = train_test_split(data)
-    (train_all, train_zx, train_zs), (test_all, test_zx, test_zs) = training_loop(train, test)
+    (train_all, train_zx, train_zs), (test_all, test_zx, test_zs) = training_loop(train, test, experiment)
 
     lr = LR()
 
-    print("unfair:")
+    print("Original:")
     train_unfair = DataTuple(x=pd.concat([train.x, train.s], axis='columns'), s=train.s, y=train.y)
     test_unfair = DataTuple(x=pd.concat([test.x, test.s], axis='columns'), s=test.s, y=test.y)
     preds_unfair = lr.run(train_unfair, test_unfair)
     results = run_metrics(preds_unfair, test_unfair, [Accuracy()], [ProbPos()])
+    experiment.log_metric("Original Accuracy", results['Accuracy'])
+    experiment.log_metric("Original P(Y=1|s=0)", results['sex_Male_0_prob_pos'])
+    experiment.log_metric("Original P(Y=1|s=1)", results['sex_Male_1_prob_pos'])
     print(results)
 
     print("fair:")
@@ -52,6 +60,9 @@ def main():
     test_fair = DataTuple(x=test_zx, s=test.s, y=test.y)
     preds_fair = lr.run(train_fair, test_fair)
     results = run_metrics(preds_fair, test_fair, [Accuracy()], [ProbPos()])
+    experiment.log_metric("Fair Accuracy", results['Accuracy'])
+    experiment.log_metric("Fair P(Y=1|s=0)", results['sex_Male_0_prob_pos'])
+    experiment.log_metric("Fair P(Y=1|s=1)", results['sex_Male_1_prob_pos'])
     print(results)
 
     print("predict s from fair representation:")
@@ -59,6 +70,7 @@ def main():
     test_fair_predict_s = DataTuple(x=test_zx, s=test.s, y=test.s)
     preds_s_fair = lr.run(train_fair_predict_s, test_fair_predict_s)
     results = run_metrics(preds_s_fair, test_fair_predict_s, [Accuracy()], [])
+    experiment.log_metric("Fair pred s", results['Accuracy'])
     print(results)
 
     print("predict s from unfair representation:")
@@ -66,6 +78,7 @@ def main():
     test_unfair_predict_s = DataTuple(x=test_zs, s=test.s, y=test.s)
     preds_s_unfair = lr.run(train_unfair_predict_s, test_unfair_predict_s)
     results = run_metrics(preds_s_unfair, test_unfair_predict_s, [Accuracy()], [])
+    experiment.log_metric("Unfair pred s", results['Accuracy'])
     print(results)
 
 
