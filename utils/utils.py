a8cd038e0170a2ab6911bce51323b414a4170b8f
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-03-21 12:49:54 +0000

    Disable printing of script content

--- utils/utils.py
+++ utils/utils.py
@@ -45,13 +45,13 @@ def get_logger(logpath, filepath, package_files=None, displaying=True, saving=Tr
         console_handler.setLevel(level)
         logger.addHandler(console_handler)
     logger.info(filepath)
-    with open(filepath, "r") as f:
-        logger.info(f.read())
+    # with open(filepath, "r") as f:
+    #     logger.info(f.read())
 
-    for f in package_files:
-        logger.info(f)
-        with open(f, "r") as package_f:
-            logger.info(package_f.read())
+    # for f in package_files:
+    #     logger.info(f)
+    #     with open(f, "r") as package_f:
+    #         logger.info(package_f.read())
 
     return StyleAdapter(logger)
 
