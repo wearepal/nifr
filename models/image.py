207f097e10ca4f413bc578ea99fa22bb665d0d29
mb715 <mb715@sussex.ac.uk>
2019-04-30 17:55:37 +0100

    Move both squeeze operations to beginning of network.

--- models/image.py
+++ models/image.py
@@ -3,20 +3,15 @@ import layers
 
 def glow(args, input_dim):
     hidden_dims = tuple(map(int, args.dims.split("-")))
-    chain = [layers.SqueezeLayer(2)]
-    input_dim = input_dim * 2 * 2
+    squeeze_factor = 4
+    chain = [layers.SqueezeLayer(squeeze_factor)]
+    # chain += [layers.SqueezeLayer(2)]
+    input_dim = input_dim * squeeze_factor**2
     for i in range(args.depth):
         if args.batch_norm:
             chain += [layers.MovingBatchNorm2d(input_dim, bn_lag=args.bn_lag)]
         if args.glow:
             chain += [layers.Invertible1x1Conv(input_dim)]
         chain += [layers.AffineCouplingLayer(input_dim, hidden_dims)]
-
-    chain += [layers.SqueezeLayer(2)]
-    # chain += [layers.InvFlatten()]
-    # chain += [layers.UnsqueezeLayer(upscale_factor=2)]
-
-    # the layers have to be mixed again:
-    chain += [layers.Invertible1x1Conv(input_dim * 2 * 2)]
 
     return layers.SequentialFlow(chain)
