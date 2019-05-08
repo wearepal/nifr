7a145d0ba782669d9bb4760e87dd644f64c18758
mb715 <mb715@sussex.ac.uk>
2019-05-08 15:45:24 +0100

    Reintroduce indie loss when using invertible discriminators (implemented as a non-invertible one)

--- models/inv_discriminators.py
+++ models/inv_discriminators.py
@@ -2,6 +2,7 @@ from collections import namedtuple
 import math
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 import layers
@@ -9,7 +10,7 @@ import models
 from utils.training_utils import fetch_model
 from .nn_discriminators import compute_log_pz
 
-Discriminators = namedtuple('Discriminators', ['s_from_zs', 'y_from_zy'])
+Discriminators = namedtuple('Discriminators', ['s_from_zs', 'y_from_zy', 's_from_zy'])
 
 
 def make_networks(args, x_dim, z_dim_flat):
@@ -32,20 +33,18 @@ def make_networks(args, x_dim, z_dim_flat):
         args.zy_dim = z_channels - args.zs_dim
         args.zn_dim = 0
 
-        if not args.meta_learn:
-            disc_y_from_zy = models.tabular_model(args, input_dim=(wh * args.zy_dim))  # logs-softmax
-            disc_y_from_zy.to(args.device)
-        else:
-            args.zy_dim = z_channels - args.zs_dim
-            args.zn_dim = 0
-
-            disc_y_from_zy = None
+        disc_y_from_zy = models.tabular_model(args, input_dim=(wh * args.zy_dim))  # logs-softmax
+        disc_y_from_zy.to(args.device)
 
         # logistic output
         disc_s_from_zs = models.tabular_model(args, input_dim=(wh * args.zs_dim))
+        disc_s_from_zy = layers.Mlp([wh * args.zy_dim] + [512, 512] + [10],
+                                    activation=nn.ReLU,
+                                    output_activation=torch.nn.LogSoftmax)
 
     disc_s_from_zs.to(args.device)
-    discs = Discriminators(s_from_zs=disc_s_from_zs, y_from_zy=disc_y_from_zy)
+    discs = Discriminators(s_from_zs=disc_s_from_zs, y_from_zy=disc_y_from_zy,
+                           s_from_zy=disc_s_from_zy)
 
     return fetch_model(args, x_dim), discs
 
@@ -72,8 +71,10 @@ def compute_loss(args, x, s, y, model, discs, return_z=False):
 
     if args.dataset == 'cmnist':
         class_loss = multi_class_loss
+        indie_loss = F.nll_loss
     else:
         class_loss = binary_class_loss
+        indie_loss = F.binary_cross_entropy_with_logits
         x = torch.cat((x, s.float()), dim=1)
 
     z, delta_log_p = whole_model(x, zero)  # run model forward
@@ -93,12 +94,15 @@ def compute_loss(args, x, s, y, model, discs, return_z=False):
         if not args.meta_learn:
             pred_y_loss = args.pred_y_weight * class_loss(zy, y)
 
+        pred_s_from_zy = discs.s_from_zy(layers.grad_reverse(zy, lambda_=args.pred_s_from_zy_weight))
+        pred_s_from_zy_loss = indie_loss(pred_s_from_zy, s, reduction='mean')
+
     if zs.size(1) > 0:
         log_pz += compute_log_pz(zs)
         pred_s_from_zs_loss = args.pred_s_from_zs_weight * class_loss(zs, s)
 
     log_px = args.log_px_weight * (log_pz - delta_log_p).mean()
-    loss = -log_px + pred_y_loss + pred_s_from_zs_loss
+    loss = -log_px + pred_y_loss + pred_s_from_zs_loss + pred_s_from_zy_loss
 
     if return_z:
         return loss, z
