6b46a2fbd61ee088b6cf3dbf3ec3963d9e8d740e
mb715 <mb715@sussex.ac.uk>
2019-05-08 19:12:04 +0100

    recursively find packages

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='FINN',
     version='0.1.0',
     author='T. Kehrenbergg, M. Bartlet, O. Tomas',
-    packages=['finn'],
+    packages=find_packages(),
     description='Invertible Networks for Learning Fair Representations',
     python_requires=">=3.6",
     install_requires=[
