3d4d7d14de173f78eec770671e0e4e2c15e2fdd4
mb715 <mb715@sussex.ac.uk>
2019-05-09 17:36:23 +0100

    Pass correct model to reconstruct

--- finn/train.py
+++ finn/train.py
@@ -103,10 +103,13 @@ def train(model, discs, optimizer, disc_optimizer, dataloader, epoch):
         log_images(SUMMARY, x, 'original_x')
 
         if ARGS.inv_disc:
-            z = discs.assemble_whole_model(model)(x[:64])
+            model_ = discs.assemble_whole_model(model)
+            z = model_(x[:64])
         else:
+            model_ = model
             z = model(x[:64])
-        recon_all, recon_y, recon_s, recon_n, recon_ys, recon_yn = reconstruct_all(ARGS, z, model)
+
+        recon_all, recon_y, recon_s, recon_n, recon_ys, recon_yn = reconstruct_all(ARGS, z, model_)
 
         log_images(SUMMARY, recon_all, 'reconstruction_all')
         log_images(SUMMARY, recon_y, 'reconstruction_y')
@@ -139,11 +142,15 @@ def validate(model, discs, val_loader):
     x_val = torch.cat((x_val, s_val), dim=1) if ARGS.dataset == 'adult' else x_val
 
     if ARGS.dataset == 'cmnist':
+
         if ARGS.inv_disc:
-            z = discs.assemble_whole_model(model)(x_val[:64])
+            model_ = discs.assemble_whole_model(model)
+            z = model_(x_val[:64])
         else:
+            model_ = model
             z = model(x_val[:64])
-        recon_all, recon_y, recon_s, recon_n, recon_ys, recon_yn = reconstruct_all(ARGS, z, model)
+
+        recon_all, recon_y, recon_s, recon_n, recon_ys, recon_yn = reconstruct_all(ARGS, z, model_)
         log_images(SUMMARY, x_val, 'original_x', train=False)
         log_images(SUMMARY, recon_all, 'reconstruction_all', train=False)
         log_images(SUMMARY, recon_y, 'reconstruction_y', train=False)
