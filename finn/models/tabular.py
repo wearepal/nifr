1129ce810463d432aaf5b1faf632f71b7b37d0a4
mb715 <mb715@sussex.ac.uk>
2019-05-10 14:29:26 +0100

    Debugging

--- finn/models/tabular.py
+++ finn/models/tabular.py
@@ -14,7 +14,7 @@ def tabular_model(args, input_dim, depth: int=None, batch_norm: bool=None):
     for i in range(_depth):
     # #     if args.glow and args.dataset == 'adult':
     # #         chain += [layers.BruteForceLayer(input_dim)]
-        chain += [layers.MaskedCouplingLayer(input_dim, hidden_dims, 'alternate', swap=i % 2 == 0)]
+        chain += [layers.MaskedCouplingLayer(input_dim, hidden_dims, 'alternate', swap=False)]
         if _batch_norm:
             chain += [layers.MovingBatchNorm1d(input_dim, bn_lag=args.bn_lag)]
     return layers.SequentialFlow(chain)
