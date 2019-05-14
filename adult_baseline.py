8f5d9b97140779cce9428fc189773467d1a1fab3
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-05-14 16:24:13 +0100

    Fix import in Adult baseline script

--- adult_baseline.py
+++ adult_baseline.py
@@ -7,7 +7,7 @@ from ethicml.evaluators.evaluate_models import run_metrics
 from ethicml.metrics import Accuracy
 from ethicml.algorithms.utils import apply_to_joined_tuple, DataTuple
 
-from finn.utils.dataloading import load_adult_data
+from finn.data.dataloading import load_adult_data
 
 
 def main():
@@ -19,8 +19,8 @@ def main():
     # D = apply_to_joined_tuple(pd.DataFrame.reset_index, D)
     D = DataTuple(x=D.x.reset_index(drop=True), s=D.s.reset_index(drop=True), y=D.y.reset_index(drop=True))
     D_star, D = train_test_split(D, train_percentage=0.1 / 0.75)
-    # ethicml_model = SVM(kernel='linear')
-    ethicml_model = Majority()
+    ethicml_model = SVM(kernel='linear')
+    # ethicml_model = Majority()
     predictions = ethicml_model.run(D_dagger, D)
     metrics = run_metrics(predictions, D, metrics=[Accuracy()], per_sens_metrics=[])
     print(f"Accuracy: {metrics['Accuracy']}")
