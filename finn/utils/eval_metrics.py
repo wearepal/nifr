829f106b635345ad86f146834584750876d3387b
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-05-14 19:41:19 +0100

    Add batch norm everywhere

--- finn/utils/eval_metrics.py
+++ finn/utils/eval_metrics.py
@@ -39,7 +39,7 @@ def train_zy_head(args, experiment, trunk, discs, train_data, val_data):
     disc_y_from_zy_copy = tabular_model(discs.args,
                                         input_dim=discs.wh * args.zy_dim,
                                         depth=2,
-                                        batch_norm=False)
+                                        batch_norm=True)
     disc_y_from_zy_copy.load_state_dict(discs.y_from_zy.state_dict())
     disc_y_from_zy_copy.to(args.device)
     disc_y_from_zy_copy.train()
