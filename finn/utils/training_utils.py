2151d911571bf5bb122109cc02f54b53c3e5a205
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-05-15 17:47:15 +0100

    Change the default "log_px_weight"

--- finn/utils/training_utils.py
+++ finn/utils/training_utils.py
@@ -57,8 +57,8 @@ def parse_arguments(raw_args=None):
     parser.add_argument('--zs-frac', type=float, default=0.33)
     parser.add_argument('--zy-frac', type=float, default=0.33)
 
-    parser.add_argument('--log-px-weight', type=float, default=1.)
-    parser.add_argument('--pred-y-weight', type=float, default=1.)
+    parser.add_argument('--log-px-weight', type=float, default=1.e-3)
+    parser.add_argument('-pyzyw', '--pred-y-weight', type=float, default=1.)
     parser.add_argument('-pszyw', '--pred-s-from-zy-weight', type=float, default=1.)
     parser.add_argument('-pszsw', '--pred-s-from-zs-weight', type=float, default=1.)
 
