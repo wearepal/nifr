be06d9d54bc397463c552d95834cfe597d35e132
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-05-17 17:59:17 +0100

    Remove y from zys discriminator

--- finn/models/nn_discriminators.py
+++ finn/models/nn_discriminators.py
@@ -5,7 +5,6 @@ import torch.nn.functional as F
 from finn import layers
 from .disc_models import linear_classifier, conv_classifier
 from .discriminator_base import DiscBase, compute_log_pz, fetch_model
-from .mnist import MnistConvNet
 
 
 class NNDisc(DiscBase):
@@ -14,19 +13,16 @@ class NNDisc(DiscBase):
         if args.dataset == 'adult':
             s_dim = 1
             self.x_s_dim = x_dim + s_dim
-            y_dim = 1
             z_channels = z_dim_flat + 1
         elif args.dataset == 'cmnist':
             s_dim = 10
             self.x_s_dim = x_dim  # s is included in x
-            y_dim = 10
             z_channels = x_dim * 16
 
         args.zs_dim = round(args.zs_frac * z_channels)
         if args.meta_learn:
             args.zy_dim = z_channels - args.zs_dim
             args.zn_dim = 0
-            disc_y_from_zys = None
         else:
             args.zy_dim = round(args.zy_frac * z_channels)
             args.zn_dim = z_channels - args.zs_dim - args.zy_dim
@@ -42,12 +38,6 @@ class NNDisc(DiscBase):
             hidden_sizes = [400, 400]
             disc_s_from_zy = layers.Mlp([args.zy_dim] + hidden_sizes + [s_dim],
                                         activation=nn.ReLU, output_activation=output_activation)
-
-            if not args.meta_learn:
-                hidden_sizes = [100, 100]
-                disc_y_from_zys = layers.Mlp([z_channels - args.zn_dim] + hidden_sizes + [y_dim],
-                                             activation=nn.ReLU, output_activation=None)
-                disc_y_from_zys.to(args.device)
         else:
             if args.nn_disc == 'linear':
                 disc_s_from_zs = linear_classifier(args.zs_dim * 7 * 7, args.s_dim)
@@ -56,19 +46,11 @@ class NNDisc(DiscBase):
                 disc_s_from_zs = conv_classifier(args.zs_dim, args.s_dim, 2)
                 disc_s_from_zy = conv_classifier(args.zy_dim, args.s_dim, 3)
 
-            if not args.meta_learn:
-                hidden_sizes = [(args.zy_dim + args.zs_dim * 8), (args.zy_dim + args.zs_dim) * 8]
-                disc_y_from_zys = MnistConvNet(args.zy_dim + args.zs_dim, y_dim,
-                                               output_activation=nn.LogSoftmax(dim=1),
-                                               hidden_sizes=hidden_sizes)
-                disc_y_from_zys.to(args.device)
-
         disc_s_from_zs.to(args.device)
         disc_s_from_zy.to(args.device)
         self.s_from_zs = disc_s_from_zs
         self.s_from_zy = disc_s_from_zy
-        self.y_from_zys = disc_y_from_zys
-        self.disc_name_list = ['s_from_zs', 's_from_zy', 'y_from_zys']  # for generating discs_dict
+        self.disc_name_list = ['s_from_zs', 's_from_zy']  # for generating discs_dict
         self.args = args
 
     @property
@@ -88,27 +70,21 @@ class NNDisc(DiscBase):
         if self.args.dataset == 'cmnist':
             # loss_fn = F.l1_loss
             loss_fn = F.nll_loss
-            class_loss_fn = F.nll_loss
         else:
             loss_fn = F.binary_cross_entropy
             x = torch.cat((x, s.float()), dim=1)
-            class_loss_fn = F.binary_cross_entropy_with_logits
 
         z, delta_logp = model(x, zero)  # run model forward
 
         log_pz = compute_log_pz(z)
         # zn = z[:, :self.args.zn_dim]
-        zs = z[:,  self.args.zn_dim: (z.size(1) - self.args.zy_dim)]
+        zs = z[:, self.args.zn_dim: (z.size(1) - self.args.zy_dim)]
         zy = z[:, (z.size(1) - self.args.zy_dim):]
         # Enforce independence between the fair representation, zy,
         #  and the sensitive attribute, s
-        pred_y_loss = z.new_zeros(1)
         pred_s_from_zy_loss = z.new_zeros(1)
         pred_s_from_zs_loss = z.new_zeros(1)
 
-        if self.y_from_zys is not None and zy.size(1) > 0 and zs.size(1) > 0 and not self.args.meta_learn:
-            pred_y_loss = (self.args.pred_y_weight
-                           * class_loss_fn(self.y_from_zys(torch.cat((zy, zs), dim=1)), y, reduction='mean'))
         if self.s_from_zy is not None and zy.size(1) > 0:
             if self.args.entropy_loss_weight != 0:
                 pred_s_from_zy = self.s_from_zy(
@@ -129,9 +105,9 @@ class NNDisc(DiscBase):
                                    * loss_fn(self.s_from_zs(zs), s, reduction='mean'))
 
         log_px = self.args.log_px_weight * (log_pz - delta_logp).mean()
-        loss = -log_px + pred_y_loss + pred_s_from_zs_loss + pred_s_from_zy_loss
+        loss = -log_px + pred_s_from_zs_loss + pred_s_from_zy_loss
 
         if return_z:
             return loss, z
-        return (loss, -log_px, pred_y_loss, self.args.pred_s_from_zy_weight * pred_s_from_zy_loss,
+        return (loss, -log_px, z.new_zeros(1), self.args.pred_s_from_zy_weight * pred_s_from_zy_loss,
                 pred_s_from_zs_loss)
