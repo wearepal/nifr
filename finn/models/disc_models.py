eeab3e2f25175bc2532fe5ec6054670fbffa8f8d
mb715 <mb715@sussex.ac.uk>
2019-05-17 14:45:24 +0100

    Remove layer norm

--- finn/models/disc_models.py
+++ finn/models/disc_models.py
@@ -7,10 +7,8 @@ def linear_classifier(in_channels, num_classes):
     return nn.Sequential(
         Flatten(),
         nn.Linear(in_channels, 512),
-        nn.LayerNorm(512),
-        nn.Softplus(),
+        nn.ReLU(inplace=True),
         nn.Linear(512, 512),
-        nn.LayerNorm(512),
-        nn.Softplus(),
+        nn.ReLU(inplace=True),
         nn.Linear(512, num_classes),
         nn.LogSoftmax(dim=1))
