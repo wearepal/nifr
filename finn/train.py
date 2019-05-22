1e6df67c0e528f5133de840a4e9f009b2c024e69
mb715 <mb715@sussex.ac.uk>
2019-05-22 14:48:39 +0100

    update disc each iter

--- finn/train.py
+++ finn/train.py
@@ -96,7 +96,7 @@ def train(model, discs, optimizer, disc_optimizer, dataloader, epoch, task_train
         pred_s_from_zs_loss_meter.update(pred_s_from_zs_loss.item())
 
         if ARGS.full_meta:
-            loss.backward()
+            loss.backward(retain_graph=True)
             disc_optimizer.step()
             disc_optimizer.zero_grad()
             epoch_loss += loss
