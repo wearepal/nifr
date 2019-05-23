785fb2064695d25be75bcac8119946565ea4e829
mb715 <mb715@sussex.ac.uk>
2019-05-23 11:06:20 +0100

    update

--- finn/train.py
+++ finn/train.py
@@ -97,7 +97,7 @@ def train(model, discs, optimizer, disc_optimizer, dataloader, epoch, task_train
         pred_s_from_zs_loss_meter.update(pred_s_from_zs_loss.item())
 
         if ARGS.full_meta:
-            epoch_loss.backward(retain_graph=True)
+            loss.backward(retain_graph=True)
             disc_optimizer.step()
             disc_optimizer.zero_grad()
             optimizer.step()
