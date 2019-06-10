38d89cfc5cc6cd44b27d7d6e7e68438ef9019782
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-06-10 11:23:30 +0100

    Update __init__.py

--- finn/layers/__init__.py
+++ finn/layers/__init__.py
@@ -3,7 +3,7 @@ from .squeeze import SqueezeLayer, UnsqueezeLayer
 from .normalization import MovingBatchNorm1d, MovingBatchNorm2d, MovingBatchNormNd, Parameter
 from .elemwise import LogitTransform, SigmoidTransform, SoftplusTransform
 from .coupling import CouplingLayer, MaskedCouplingLayer, AffineCouplingLayer
-from .glow import BruteForceLayer, Invertible1x1Conv, ActNorm
+from .glow import BruteForceLayer, Invertible1x1Conv, ActNorm, ActNormNoData
 from .norm_flows import PlanarFlow
 from .layer_utils import InvFlatten, Flatten, Identity
 from .mlp import Mlp
