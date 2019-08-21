77920e528d1a46621c237253f69eb300174524b6
mb715 <mb715@sussex.ac.uk>
2019-08-21 16:13:56 +0100

    pytest

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,5 @@
 """Run all tests"""
-import torch
+import pytest
 
-x = torch.tensor([1, 2, 3, 4, 5])
-inds = [2, 3]
-x[inds] = x[inds][torch.randperm(len(inds))]
-print(x)
+
+pytest.main(['-vv', 'tests/'])
