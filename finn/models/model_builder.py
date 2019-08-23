Fix input dimension computation.

finn/models/model_builder.py
@@ -55,11 +55,13 @@ def build_discriminator(args, input_shape, model_fn, model_kwargs, flatten):
         h, w = input_shape[1:]
         if not args.learn_mask:
             in_dim *= args.squeeze_factor ** 2
-            in_dim = int(in_dim - (args.zs_frac * in_dim))
-            h /= args.squeeze_factor
-            w /= args.squeeze_factor
+            zs_dim = int(args.zs_frac * in_dim)
+            in_dim = in_dim - zs_dim
+            h //= args.squeeze_factor
+            w //= args.squeeze_factor
         if flatten:
             in_dim = int(np.product((in_dim, h, w)))
+            print(in_dim)
 
     n_classes = args.y_dim if args.y_dim > 1 else 2
     discriminator = Classifier(