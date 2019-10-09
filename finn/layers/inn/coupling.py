d32433877aaf20bf894f413ef8052f6794800634
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2019-10-09 20:29:29 +0100

    bring back resnet 2.0

--- finn/layers/inn/coupling.py
+++ finn/layers/inn/coupling.py
@@ -38,21 +38,14 @@ class AffineCouplingLayer(CouplingLayer):
         super().__init__()
         self.d = in_channels - round(pcnt_to_transform * in_channels)
 
-        # self.net_s_t = ConvResidualNet(
-        #         in_channels=self.d,
-        #         out_channels=(in_channels - self.d) * 2,
-        #         hidden_channels=hidden_channels,
-        #         num_blocks=num_blocks,
-        #         activation=F.relu,
-        #         dropout_probability=0,
-        #         use_batch_norm=False)
-
-        self.net_s_t = BottleneckConvBlock(
-            in_channels=self.d,
-            hidden_channels=hidden_channels,
-            out_channels=(in_channels - self.d) * 2,
-            use_bn=False,
-        )
+        self.net_s_t = ConvResidualNet(
+                in_channels=self.d,
+                out_channels=(in_channels - self.d) * 2,
+                hidden_channels=hidden_channels,
+                num_blocks=num_blocks,
+                activation=F.relu,
+                dropout_probability=0,
+                use_batch_norm=False)
 
     def logdetjac(self, scale):
         return sum_except_batch(torch.log(scale), keepdim=True)
@@ -101,15 +94,6 @@ class AdditiveCouplingLayer(CouplingLayer):
                 activation=F.relu,
                 dropout_probability=0,
                 use_batch_norm=False)
-
-        # self.net_t = ConvResidualNet(
-        #         in_channels=self.d,
-        #         out_channels=(in_channels - self.d),
-        #         hidden_channels=hidden_channels,
-        #         num_blocks=num_blocks,
-        #         activation=F.relu,
-        #         dropout_probability=0,
-        #         use_batch_norm=False)
 
     def logdetjac(self):
         return 0
