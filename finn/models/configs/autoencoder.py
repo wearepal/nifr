821ba00bfd8a47715e19a99f5753a5ca01812220
mb715 <mb715@sussex.ac.uk>
2019-10-11 14:54:09 +0100

    Change sequence of operations in AutoEncoder blocks.

--- finn/models/configs/autoencoder.py
+++ finn/models/configs/autoencoder.py
@@ -29,17 +29,17 @@ def conv_autoencoder(input_shape, initial_hidden_channels, levels, encoded_dim):
             c_in = c_out
             c_out *= 2
 
-        encoder += _down_conv_block(c_in, c_out, kernel_size=3, stride=1, padding=1)
-        encoder += _down_conv_block(c_out, c_out, kernel_size=4, stride=2, padding=1)
+        encoder += [_down_conv_block(c_in, c_out, kernel_size=3, stride=1, padding=1)]
+        encoder += [_down_conv_block(c_out, c_out, kernel_size=4, stride=2, padding=1)]
 
-        decoder += _up_conv_block(c_out, c_in, kernel_size=3, stride=1, padding=1, output_padding=0)
-        decoder += _up_conv_block(c_out, c_out, kernel_size=4, stride=2, padding=1, output_padding=0)
+        decoder += [_down_conv_block(c_out, c_in, kernel_size=3, stride=1, padding=1)]
+        decoder += [_up_conv_block(c_out, c_out, kernel_size=4, stride=2, padding=1, output_padding=0)]
 
         h //= 2
         w //= 2
 
-    encoder += [_down_conv_block(c_out, encoded_dim, 1, 1, 0)]
-    decoder += [_up_conv_block(encoded_dim, c_out, 1, 1, 0, 0)]
+    encoder += [_down_conv_block(c_out, encoded_dim, kernel_size=3, stride=1, padding=1)]
+    decoder += [_down_conv_block(encoded_dim, c_out, kernel_size=3, stride=1, padding=1)]
     decoder = decoder[::-1]
 
     encoder = nn.Sequential(*encoder)
