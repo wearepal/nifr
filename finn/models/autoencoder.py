deffbf08b38ee5ab94db84b2194baeecd1ea462f
mb715 <mb715@sussex.ac.uk>
2019-10-15 18:06:38 +0100

    Return only the sample when encoding with vae after training.

--- finn/models/autoencoder.py
+++ finn/models/autoencoder.py
@@ -58,5 +58,72 @@ class AutoEncoder(nn.Module):
                     loss = self.routine(x, loss_fn=loss_fn)
                     loss.backward()
                     self.step()
+
                     pbar.update()
                     pbar.set_postfix(AE_loss=loss.detach().cpu().numpy())
+
+
+class VAE(AutoEncoder):
+
+    def __init__(self, encoder, decoder, kl_weight=0.1, optimizer_args=None):
+        super(AutoEncoder, self).__init__()
+
+        super().__init__(encoder=encoder,
+                         decoder=decoder,
+                         optimizer_args=optimizer_args)
+        self.encoder: ModelBase = ModelBase(encoder, optimizer_args=optimizer_args)
+        self.decoder: ModelBase = ModelBase(decoder, optimizer_args=optimizer_args)
+
+        self.prior = td.Normal(0, 1)
+        self.posterior_fn = td.Normal
+        self.kl_weight = kl_weight
+
+    def compute_kl(self, sample, posterior):
+        log_p = self.prior.log_prob(sample)
+        log_q = posterior.log_prob(sample)
+
+        kld = log_q.sum() - log_p.sum()
+        kld /= sample.size(0)
+
+        return kld
+
+    def encode(self, x, stochastic=True, return_posterior=False):
+        loc, scale = self.encoder(x).chunk(2, dim=1)
+
+        if stochastic or return_posterior:
+            scale = F.softplus(scale)
+            posterior = self.posterior_fn(loc, scale)
+
+        sample = posterior.rsample() if stochastic else loc
+
+        if return_posterior:
+            return sample, posterior
+        else:
+            return sample
+
+    def routine(self, x, recon_loss_fn, stochastic=True):
+        sample, posterior = self.encode(x, stochastic=stochastic, return_posterior=True)
+        kld = self.compute_kl(sample, posterior)
+        recon = self.decoder(sample)
+        recon_loss = recon_loss_fn(recon, x)
+
+        loss = recon_loss + self.kl_weight * kld
+
+        return loss
+
+    def fit(self, train_data, epochs, device, loss_fn=nn.MSELoss()):
+
+        self.train()
+
+        with trange(epochs) as pbar:
+            for epoch in pbar:
+
+                for x, _, _ in train_data:
+
+                    x = x.to(device)
+
+                    self.zero_grad()
+                    loss = self.routine(x, recon_loss_fn=loss_fn)
+                    loss.backward()
+                    self.step()
+                pbar.set_postfix(AE_loss=loss.detach().cpu().numpy())
