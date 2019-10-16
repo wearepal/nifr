f7d76a73d51e1f72d15ccd8ce872ac98054d03f3
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2019-10-16 16:06:57 +0100

    Move bash script to root.

--- eval_naive_cmnist.sh
+++ eval_naive_cmnist.sh
@@ -6,7 +6,7 @@ for greyscale in "False" "True"
 do
 for scale in "0.0" "0.01" "0.02" "0.03" "0.04" "0.05"
 do
-	python python -m naive \
+	python python finn/baselines/naive.py \
 		--dataset cmnist --scale $scale --greyscale $greyscale \
 		--lr 1e-3 --batch-size 128 --weight-decay 0 --epochs $epochs \
 		--padding 2 --pred-s False
