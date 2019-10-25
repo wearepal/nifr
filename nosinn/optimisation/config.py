c24317a20d7b33aa79cd4e20bb395804ed75c1ff
mb715 <mb715@sussex.ac.uk>
2019-10-25 14:26:51 +0100

    Hyphens in CL arguments.  Please enter the commit message for your changes. Lines starting

--- nosinn/optimisation/config.py
+++ nosinn/optimisation/config.py
@@ -198,8 +198,8 @@ def vae_args(raw_args=None):
 
     # VAEsettings
     parser.add_argument("--levels", type=int, default=4)
-    parser.add_argument("--enc_y_dim", type=int, default=64)
-    parser.add_argument("--enc_s_dim", type=int, default=8)
+    parser.add_argument("--enc-y-dim", type=int, default=64)
+    parser.add_argument("--enc-s-dim", type=int, default=8)
     parser.add_argument("--cond-decoder", type=eval, choices=[True, False], default=False)
     parser.add_argument("--init-channels", type=int, default=32)
     parser.add_argument("--recon-loss", type=str, choices=["l1", "l2", "huber", "ce"], default="l2")
