9004a11b2a198042416211e895f320ee1f517c0d
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-10-25 15:58:55 +0100

    Add some types

--- nosinn/optimisation/evaluation.py
+++ nosinn/optimisation/evaluation.py
@@ -4,18 +4,80 @@ from argparse import Namespace
 import pandas as pd
 import wandb
 
+import torch
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+
 from ethicml.algorithms.inprocess import LR
 from ethicml.evaluators import run_metrics
 from ethicml.metrics import Accuracy, Theil, ProbPos, TPR, TNR, PPV, NMI
 from ethicml.utility import DataTuple
 
-import torch
-from torch.utils.data import DataLoader, Dataset, TensorDataset
-
-from nosinn.data import get_data_tuples
+from nosinn.data import get_data_tuples, DatasetTriplet
 from nosinn.models.classifier import Classifier
 from nosinn.models.configs.classifiers import fc_net, mp_32x32_net, mp_64x64_net
 from nosinn.models.inn import BipartiteInn
+from .utils import log_images
+
+
+def log_sample_images(data, name, step):
+    data_loader = DataLoader(data, shuffle=False, batch_size=64)
+    x, _, _ = next(iter(data_loader))
+    log_images(x, f"Samples from {name}", prefix="eval", step=step)
+
+
+def log_metrics(
+    args,
+    model,
+    data,
+    step,
+    quick_eval=True,
+    save_to_csv: Optional[Path] = None,
+    check_originals: bool = False,
+):
+    """Compute and log a variety of metrics"""
+    ethicml_model = LR()
+
+    print("Encoding task dataset...")
+    task_repr = encode_dataset(args, data.task, model, recon=True, subdir="task")
+    print("Encoding task train dataset...")
+    task_train_repr = encode_dataset(args, data.task_train, model, recon=True, subdir="task_train")
+
+    print("\nComputing metrics...")
+    evaluate(
+        args,
+        step,
+        task_train_repr["xy"],
+        task_repr["xy"],
+        name="xy",
+        train_on_recon=True,
+        pred_s=False,
+        save_to_csv=save_to_csv,
+    )
+    # print("===> Predict y from xy")
+    # evaluate(args, experiment, repr.task_train['x'], repr.task['x'], name='xy', pred_s=False)
+    # print("===> Predict s from xy")
+    # evaluate(args, experiment, task_train_repr['xy'], task_repr['xy'], name='xy', pred_s=True)
+
+    if quick_eval:
+        log_sample_images(data.task_train, "task_train", step=step)
+    else:
+
+        if args.dataset == "adult":
+            task_data, task_train_data = get_data_tuples(data.task, data.task_train)
+            data = DatasetTriplet(
+                pretrain=None,
+                task=task_data,
+                task_train=task_train_data,
+                input_dim=data.input_dim,
+                output_dim=data.output_dim,
+            )
+
+        # ===========================================================================
+
+        evaluate(args, step, task_train_repr["zy"], task_repr["zy"], name="zy")
+        evaluate(args, step, task_train_repr["zs"], task_repr["zs"], name="zs")
+        evaluate(args, step, task_train_repr["xy"], task_repr["xy"], name="xy")
+        evaluate(args, step, task_train_repr["xs"], task_repr["xs"], name="xs")
 
 
 def compute_metrics(predictions, actual, name, step, run_all=False) -> Dict[str, float]:
