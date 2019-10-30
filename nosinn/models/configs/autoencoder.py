45c618ef3645c967e2e0867e7165be844083ba30
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2019-10-30 18:58:36 +0000

    kernel size.

--- nosinn/models/configs/autoencoder.py
+++ nosinn/models/configs/autoencoder.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 class _ResidualDownBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1):
+    def __init__(self, inplanes, planes, kernel_size, stride=1):
         super().__init__()
         self.bn1 = nn.BatchNorm2d(inplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=kernel_size, stride=stride,
                                padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
@@ -40,11 +40,11 @@ class _ResidualDownBlock(nn.Module):
 class _ResidualUpBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, kernel_size, stride=1, downsample=None):
         super().__init__()
         self.bn1 = nn.BatchNorm2d(inplanes)
         self.relu = nn.ReLU(inplace=True)
-        self.conv1 = nn.ConvTranspose2d(inplanes, planes, kernel_size=3, stride=stride,
+        self.conv1 = nn.ConvTranspose2d(inplanes, planes, kernel_size=kernel_size, stride=stride,
                                         padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1,
@@ -109,15 +109,15 @@ def conv_autoencoder(input_shape, initial_hidden_channels, levels, encoding_dim,
 
         # encoder += [gated_conv(c_in, c_out, kernel_size=3, stride=1, padding=1)]
         # encoder += [gated_conv(c_out, c_out, kernel_size=4, stride=2, padding=1)]
-        encoder += [_ResidualDownBlock(c_in, c_out)]
-        encoder += [_ResidualDownBlock(c_out, c_out, stride=2)]
+        encoder += [_ResidualDownBlock(c_in, c_out, kernel_size=3)]
+        encoder += [_ResidualDownBlock(c_out, c_out, stride=2, kernel_size=4)]
 
         # decoder += [gated_conv(c_out, c_in, kernel_size=3, stride=1, padding=1)]
         # decoder += [
         #     gated_up_conv(c_out, c_out, kernel_size=4, stride=2, padding=1, output_padding=0)
         # ]
-        decoder += [_ResidualDownBlock(c_out, c_in)]
-        decoder += [_ResidualUpBlock(c_out, c_out, stride=2)]
+        decoder += [_ResidualDownBlock(c_out, c_in, kernel_size=3)]
+        decoder += [_ResidualUpBlock(c_out, c_out, stride=2, kernel_size=4)]
 
         h //= 2
         w //= 2
