62084e0e07511d4f28a09ab6169072522f7150db
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2019-10-30 15:02:39 +0000

    Typo in comment.

--- nosinn/layers/conv.py
+++ nosinn/layers/conv.py
@@ -21,11 +21,12 @@ class BottleneckConvBlock(nn.Module):
             *_sub_block(hidden_channels, hidden_channels, 1),
             nn.Conv2d(hidden_channels, out_channels, kernel_size=3, stride=1, padding=1),
         ]
-        # Initialize final kernel to zero so the coupling layer initially performs
-        # and identity mapping
+
         for m in layers[:-1]:
             if hasattr(m, "weight"):
                 nn.init.xavier_normal_(m.weight)
+        # Initialize final kernel to zero so the coupling layer initially performs
+        # an identity mapping
         nn.init.uniform_(layers[-1].weight, a=-1e-3, b=1e-3)
 
         self.sub_blocks = nn.Sequential(*layers)
