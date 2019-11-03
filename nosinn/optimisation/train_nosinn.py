a08350e80100f3e8fcc41eae115cf1d76f97df59
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2019-11-03 22:14:31 +0000

    Remove stop gradient.

--- nosinn/optimisation/train_nosinn.py
+++ nosinn/optimisation/train_nosinn.py
@@ -79,7 +79,7 @@ def train(inn, discriminator, dataloader, epoch: int) -> int:
         recon_loss = x.new_zeros((1))
         if ARGS.train_on_recon:
             enc_y_m = torch.cat([enc_y, torch.zeros_like(enc_s)], dim=1)
-            enc_y = inn.invert(enc_y_m.detach())
+            enc_y = inn.invert(enc_y_m)
             if ARGS.recon_stability_weight > 0:
                 recon_loss = F.l1_loss(enc_y, x)
             # enc_y = enc_y.clamp(min=0, max=1)
