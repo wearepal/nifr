0a139901f6a9ea9049238d9f307b79766cfb1bb3
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-11-07 19:58:17 +0000

    Compute validation loss correctly

--- eval_loop_adult_ae_on_recon.sh
+++ eval_loop_adult_ae_on_recon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-topic="adult_ae"
+topic="adult_ae_on_recon"
 
 for mix_fact in "0.0" "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9" "1.0"
 do
@@ -18,21 +18,21 @@ python start.py \
 --coupling-depth 1 \
 --dataset adult \
 --disc-hidden-dims 256 \
---disc-lr 0.0003 \
+--disc-lr 3e-4 \
 --drop-native True \
 --epochs 200 \
 --gamma 0.96 \
 --glow True \
 --input-noise False \
 --level-depth 1 \
---lr 0.001 \
---nll-weight 10 \
+--lr 1e-3 \
+--nll-weight 1 \
 --pred-s-weight 3e-3 \
 --results-csv ${topic}.csv \
 --scaling add2_sigmoid \
 --super-val True \
 --task-mixing-factor $mix_fact \
---train-on-recon False \
+--train-on-recon True \
 --vae False \
 --val-freq 20 \
 --weight-decay 1e-6 \
