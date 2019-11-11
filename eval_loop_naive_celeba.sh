52fbd700f82fde6ed0ea11d6dd774be49e344ab8
Myles Bartlett <42840629+msbartlett@users.noreply.github.com>
2019-11-11 22:08:45 +0000

    Add script to run celeba naive baseline classifier.

---
+++ eval_loop_naive_celeba.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+epochs="40"
+
+for mf in "0.0" "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9" "1.0"
+do
+	python run_naive_baseline.py \
+		--dataset celeba --task-mixing-factor $mf \
+		--lr 1e-3 --batch-size 128 --weight-decay 0 --epochs $epochs \
+		--pred-s False
+done
+
