531e09737dc150a0825814373ea1149adeefd3f9
mb715 <mb715@sussex.ac.uk>
2019-11-12 19:19:14 +0000

    Corect save path.

--- eval_loop_five_kims_celeba.sh
+++ eval_loop_five_kims_celeba.sh
@@ -6,6 +6,6 @@ do
 		--dataset celeba --task-mixing-factor $mf \
 		--lr 1e-3 --batch-size 128 --weight-decay 0 --epochs $epochs \
         --entropy-weight 0.01 \
-        --results-csv five_kims_adult
+        --results-csv five_kims_celeba
 done
 
