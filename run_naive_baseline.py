fcdb84bf69f0f18691a66ccb2a49e62af965fc94
thomkeh <7741417+thomkeh@users.noreply.github.com>
2019-12-06 17:23:57 +0000

    Update baseline code so that it can run

--- run_naive_baseline.py
+++ run_naive_baseline.py
@@ -1,101 +1,43 @@
-import argparse
 from pathlib import Path
 
 import pandas as pd
 import torch
+from torch.utils.data import DataLoader
 from ethicml.evaluators import run_metrics
 from ethicml.metrics import Accuracy, ProbPos, TPR
 from ethicml.utility import DataTuple
-from torch.utils.data import DataLoader
 
 from nosinn.data import load_dataset
 from nosinn.models import Classifier
-from nosinn.models.configs.classifiers import mp_32x32_net, fc_net, mp_64x64_net, resnet_50_ft
+from nosinn.models.configs.classifiers import mp_32x32_net, fc_net, mp_64x64_net
 from nosinn.optimisation import get_data_dim
 from nosinn.utils import random_seed
+from nosinn.configs import SharedArgs
 
 
-def parse_arguments():
-    parser = argparse.ArgumentParser()
+class NaiveArgs(SharedArgs):
     # General data set settings
-    parser.add_argument("--dataset", choices=["adult", "cmnist", "celeba"], default="cmnist")
-    parser.add_argument(
-        "--data-pcnt",
-        type=float,
-        metavar="P",
-        default=1.0,
-        help="data %% should be a real value > 0, and up to 1",
-    )
-    parser.add_argument(
-        "--task-mixing-factor",
-        type=float,
-        metavar="P",
-        default=0.0,
-        help="How much of meta train should be mixed into task train.",
-    )
-    parser.add_argument(
-        "--pretrain",
-        type=eval,
-        default=True,
-        choices=[True, False],
-        help="Whether to perform unsupervised pre-training.",
-    )
-    parser.add_argument("--pretrain-pcnt", type=float, default=0.4)
-    parser.add_argument("--task-pcnt", type=float, default=0.2)
-    parser.add_argument("--data-split-seed", type=int, default=888)
-
-    # Adult data set feature settings
-    parser.add_argument("--drop-native", type=eval, default=True, choices=[True, False])
-    parser.add_argument("--drop-discrete", type=eval, default=False)
-
-    # Colored MNIST settings
-    parser.add_argument("--scale", type=float, default=0.02)
-    parser.add_argument("-bg", "--background", type=eval, default=False, choices=[True, False])
-    parser.add_argument("--black", type=eval, default=True, choices=[True, False])
-    parser.add_argument("--binarize", type=eval, default=True, choices=[True, False])
-    parser.add_argument("--rotate-data", type=eval, default=False, choices=[True, False])
-    parser.add_argument("--shift-data", type=eval, default=False, choices=[True, False])
-    parser.add_argument("--padding", type=int, default=2)
-    parser.add_argument("--quant-level", type=int, default=8, choices=[3, 5, 8])
-    parser.add_argument("--input-noise", type=eval, default=True, choices=[True, False])
-    parser.add_argument(
-        "--greyscale",
-        type=eval,
-        choices=[True, False],
-        default=True,
-        help="Whether to grescale the images. Only applies to coloured MNIST.",
-    )
-    # CelebA settings
-    parser.add_argument("--celeba-sens-attr", type=str, default="Male", choices=["Male", "Young"])
-    parser.add_argument(
-        "--celeba-target-attr", type=str, default="Smiling", choices=["Smiling", "Attractive"]
-    )
+    greyscale: bool = True
 
     # Optimization settings
-    parser.add_argument("--epochs", type=int, default=40)
-    parser.add_argument("--batch-size", type=int, default=128)
-    parser.add_argument("--test-batch-size", type=int, default=1000)
-    parser.add_argument("--lr", type=float, default=1e-3)
-    parser.add_argument("--weight-decay", type=float, default=0)
+    epochs: int = 40
+    test_batch_size: int = 1000
+    lr: float = 1e-3
 
     # Misc settings
-    parser.add_argument("--pred-s", type=eval, default=False, choices=[True, False])
-    parser.add_argument("--root", type=str, default="data")
-    parser.add_argument("--seed", type=int, default=42)
-    parser.add_argument("--gpu", type=int, default=0, help="which GPU to use (if available)")
-    parser.add_argument("--save", type=str, default="nosinn/baselines/experiments")
-
-    return parser.parse_args()
+    pred_s: bool = False
 
 
-if __name__ == "__main__":
-
-    args = parse_arguments()
+def main():
+    args = NaiveArgs()
+    args.parse_args()
+    print(args)
 
     use_gpu = torch.cuda.is_available() and not args.gpu < 0
     random_seed(args.seed, use_gpu)
 
-    args.device = torch.device(f"cuda:{args.gpu}" if use_gpu else "cpu")
+    device = torch.device(f"cuda:{args.gpu}" if use_gpu else "cpu")
+    print(f"Running on {device}")
 
     datasets = load_dataset(args)
 
@@ -108,7 +50,7 @@ if __name__ == "__main__":
 
         def adult_fc_net(in_dim, target_dim):
             encoder = fc_net(in_dim, 35, hidden_dims=[35])
-            classifier = torch.nn.Linear(35, args.y_dim)
+            classifier = torch.nn.Linear(35, datasets.y_dim)
             return torch.nn.Sequential(encoder, classifier)
 
         classifier_fn = adult_fc_net
@@ -121,20 +63,20 @@ if __name__ == "__main__":
     )
 
     input_shape = get_data_dim(train_loader)
-    target_dim = args.s_dim if args.pred_s else args.y_dim
+    target_dim = datasets.s_dim if args.pred_s else datasets.y_dim
 
     classifier: Classifier = Classifier(
         classifier_fn(input_shape[0], target_dim),
         num_classes=2 if target_dim == 1 else target_dim,
         optimizer_kwargs={"lr": args.lr, "weight_decay": args.weight_decay},
     )
-    classifier.to(args.device)
+    classifier.to(device)
 
     classifier.fit(
-        train_loader, test_data=test_loader, epochs=args.epochs, device=args.device, pred_s=False
+        train_loader, test_data=test_loader, epochs=args.epochs, device=device, pred_s=False
     )
 
-    preds, ground_truths, sens = classifier.predict_dataset(test_data, device=args.device)
+    preds, ground_truths, sens = classifier.predict_dataset(test_data, device=device)
     preds = pd.DataFrame(preds, columns=["labels"])
     ground_truths = DataTuple(
         x=pd.DataFrame(sens, columns=["sens"]),
@@ -160,9 +102,8 @@ if __name__ == "__main__":
     print()
 
     if args.save is not None:
-        save_to_csv = Path(args.save)
-        if not save_to_csv.exists():
-            save_to_csv.mkdir(exist_ok=True)
+        save_to_csv = Path(args.save_dir)
+        save_to_csv.mkdir(exist_ok=True)
 
         assert isinstance(save_to_csv, Path)
         results_path = save_to_csv / full_name
@@ -182,3 +123,7 @@ if __name__ == "__main__":
                 else:
                     f.write(",".join(["Mix_fact"] + [str(k) for k in metrics.keys()]) + "\n")
                 f.write(value_list + "\n")
+
+
+if __name__ == "__main__":
+    main()
