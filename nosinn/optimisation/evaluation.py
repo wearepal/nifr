8a64b5b8fd384eae26fe91a541e9919edfc3c113
mb715 <mb715@sussex.ac.uk>
2019-12-10 17:05:08 +0000

    Debugging.

--- nosinn/optimisation/evaluation.py
+++ nosinn/optimisation/evaluation.py
@@ -67,8 +67,8 @@ def log_metrics(
 
         if image_orig.dim() == 3:
             clf.cpu()
-            target_orig = target_orig.view(0)
-            target_deb = target_deb.view(0)
+            # target_orig = target_orig
+            # target_deb = target_deb.view(0)
             import pdb; pdb.set_trace()
             feat_attr_map_orig = get_image_attribution(image_orig, target_orig, clf)
             feat_attr_map_orig.savefig(f"{args.save_dir}/feat_attr_map_orig.png")
