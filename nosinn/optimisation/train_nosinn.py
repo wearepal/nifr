0b3503a38b0f3c37bbd5069463453b67ead756f0
mb715 <mb715@sussex.ac.uk>
2019-12-18 18:47:34 +0000

    tensor shape.

--- nosinn/optimisation/train_nosinn.py
+++ nosinn/optimisation/train_nosinn.py
@@ -99,7 +99,7 @@ def compute_loss(
             recon_loss = ARGS.recon_stability_weight * F.mse_loss(recon, recon_target)
 
     enc_y = grad_reverse(enc_y)
-    disc_loss = x.new_zeros(())
+    disc_loss = x.new_zeros(1)
     for disc in disc_ensemble:
         disc_loss += disc.routine(enc_y, s)[0]
 
