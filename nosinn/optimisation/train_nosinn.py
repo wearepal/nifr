29da3f8f31889cdfdfa6fa7bb0fb9b268f4beaeb
Myles Bartlett <mb715@m900328.inf.susx.ac.uk>
2020-02-13 12:47:03 +0000

    Move logging code to trigger conditionally within val loop.

--- nosinn/optimisation/train_nosinn.py
+++ nosinn/optimisation/train_nosinn.py
@@ -173,7 +173,7 @@ def validate(inn: PartitionedInn, disc_ensemble: nn.ModuleList, val_loader, itr:
 
     with torch.set_grad_enabled(False):
         loss_meter = AverageMeter()
-        for x_val, s_val, y_val in val_loader:
+        for val_itr, (x_val, s_val, y_val) in enumerate(val_loader):
 
             x_val, s_val, y_val = to_device(x_val, s_val, y_val)
 
@@ -181,20 +181,21 @@ def validate(inn: PartitionedInn, disc_ensemble: nn.ModuleList, val_loader, itr:
 
             loss_meter.update(logging_dict["Validation loss"], n=x_val.size(0))
 
-        wandb_log(ARGS, {"Loss": loss_meter.avg}, step=itr)
+            if val_itr == 0:
+                if ARGS.dataset in ("cmnist", "celeba"):
+                    log_recons(inn, x_val, itr, prefix="test")
+            else:
+                z = inn(x_val[:1000])
+                _, recon_y, recon_s = inn.decode(z, partials=True)
+                log_images(ARGS, x_val, "original_x", prefix="test", step=itr)
+                log_images(ARGS, recon_y, "reconstruction_yn", prefix="test", step=itr)
+                log_images(ARGS, recon_s, "reconstruction_yn", prefix="test", step=itr)
+                x_recon = inn(inn(x_val), reverse=True)
+                x_diff = (x_recon - x_val).abs().mean().item()
+                print(f"MAE of x and reconstructed x: {x_diff}")
+                wandb_log(ARGS, {"reconstruction MAE": x_diff}, step=itr)
 
-        if ARGS.dataset in ("cmnist", "celeba"):
-            log_recons(inn, x_val, itr, prefix="test")
-        else:
-            z = inn(x_val[:1000])
-            recon_all, recon_y, recon_s = inn.decode(z, partials=True)
-            log_images(ARGS, x_val, "original_x", prefix="test", step=itr)
-            log_images(ARGS, recon_y, "reconstruction_yn", prefix="test", step=itr)
-            log_images(ARGS, recon_s, "reconstruction_yn", prefix="test", step=itr)
-            x_recon = inn(inn(x_val), reverse=True)
-            x_diff = (x_recon - x_val).abs().mean().item()
-            print(f"MAE of x and reconstructed x: {x_diff}")
-            wandb_log(ARGS, {"reconstruction MAE": x_diff}, step=itr)
+        wandb_log(ARGS, {"Loss": loss_meter.avg}, step=itr)
 
     return loss_meter.avg
 
