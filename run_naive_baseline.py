555ecb1ca8bdfcd5ff6da0e62f24569c84287e7d
Myles Bartlett <mb715@m900328.inf.susx.ac.uk>
2020-02-20 22:01:54 +0000

    Integrate Kamiran into baseline script.

--- run_naive_baseline.py
+++ run_naive_baseline.py
@@ -1,21 +1,42 @@
+from abc import ABC, abstractstaticmethod
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 import torch
-from torch.utils.data import DataLoader
+from ethicml.algorithms import compute_instance_weights
 from ethicml.evaluators import run_metrics
-from ethicml.metrics import Accuracy, ProbPos, TPR
-from ethicml.utility import DataTuple
+from ethicml.metrics import TPR, Accuracy, ProbPos
+from ethicml.utility import DataTuple, Prediction
+from torch.utils.data import DataLoader, Dataset, TensorDataset
+from tqdm import trange
+from typing_extensions import Literal
 
+from nosinn.configs import SharedArgs
 from nosinn.data import load_dataset
 from nosinn.models import Classifier
-from nosinn.models.configs.classifiers import mp_32x32_net, fc_net, mp_64x64_net
+from nosinn.models.configs.classifiers import fc_net, mp_32x32_net, mp_64x64_net
 from nosinn.optimisation import get_data_dim
 from nosinn.utils import random_seed
-from nosinn.configs import SharedArgs
+
+BASELINE_METHODS = Literal["naive", "kamiran"]
 
 
-class NaiveArgs(SharedArgs):
+class IntanceWeightedDataset(Dataset):
+    def __init__(self, dataset, instance_weights):
+        super().__init__()
+        self.dataset = dataset
+        self.instance_weights = instance_weights
+
+    def __getitem__(self, index):
+        data = self.dataset[index]
+        if not isinstance(data, tuple):
+            data = (data,)
+        iw = self.instance_weights[index]
+        return data + (iw,)
+
+
+class BaselineArgs(SharedArgs):
     # General data set settings
     greyscale: bool = True
 
@@ -25,13 +46,88 @@ class NaiveArgs(SharedArgs):
     lr: float = 1e-3
 
     # Misc settings
+    method: BASELINE_METHODS = "naive"
     pred_s: bool = False
 
+    def process_args(self):
+        if self.method == "kamiran":
+            if self.dataset == "cmnist":
+                raise ValueError("Kamiran & Calders reweighting scheme can only be used with binary sensitive and target attributes.")
+            elif self.task_mixing_factor % 1 == 0:
+                raise ValueError("Kamiran & Calders reweighting scheme can only be used when there is at least one sample available for each sensitive/target attribute combination.")
 
-def main():
-    args = NaiveArgs(explicit_bool=True, underscores_to_dashes=True)
-    args.parse_args()
-    print(args)
+        return super().process_args()
+
+
+def get_instance_weights(dataset):
+    s_all, y_all = [], []
+    for _, s, y in DataLoader(dataset):
+        s_all.append(s.numpy())
+        y_all.append(y.numpy())
+
+    s_all = np.vstack(s_all)
+    y_all = np.vstack(y_all)
+
+    labels = DataTuple(
+        x=None, s=pd.DataFrame(s_all, columns=["sens"]), y=pd.DataFrame(y_all, columns=["labels"]),
+    )
+
+    instance_weights = torch.as_tenosr(compute_instance_weights(labels))
+    instance_weights = TensorDataset(instance_weights)
+
+    return instance_weights
+
+
+class Trainer(ABC):
+    @abstractstaticmethod
+    def __call__(classifier, train_loader, test_loader, epochs, device, pred_s=False):
+        ...
+
+
+class TrainNaive(Trainer):
+    @staticmethod
+    def __call__(classifier, train_loader, test_loader, epochs, device, pred_s=False):
+        pbar = trange(epochs)
+        for epoch in pbar:
+            classifier.train()
+            for x, s, y in train_loader:
+                if pred_s:
+                    target = s
+                else:
+                    target = y
+                x = x.to(device)
+                target = target.to(device)
+
+                classifier.zero_grad()
+                loss, acc = classifier.routine(x, target)
+                loss.backward()
+                classifier.step()
+        pbar.close()
+
+
+class TrainKamiran(Trainer):
+    @staticmethod
+    def __call__(classifier, train_loader, test_loader, epochs, device, pred_s=False):
+        pbar = trange(epochs)
+        for epoch in pbar:
+            classifier.train()
+            for x, s, y, iw in train_loader:
+                if pred_s:
+                    target = s
+                else:
+                    target = y
+                x = x.to(device)
+                target = target.to(device)
+
+                classifier.zero_grad()
+                loss, acc = classifier.routine(x, target)
+                loss = iw * loss
+                loss.backward()
+                classifier.step()
+        pbar.close()
+
+
+def run_baseline(args):
 
     use_gpu = torch.cuda.is_available() and not args.gpu < 0
     random_seed(args.seed, use_gpu)
@@ -39,23 +135,15 @@ def main():
     device = torch.device(f"cuda:{args.gpu}" if use_gpu else "cpu")
     print(f"Running on {device}")
 
+    #  Load the datasets and wrap with dataloaders
     datasets = load_dataset(args)
 
     train_data = datasets.task_train
     test_data = datasets.task
 
-    if args.dataset == "cmnist":
-        classifier_fn = mp_32x32_net
-    elif args.dataset == "adult":
-
-        def adult_fc_net(in_dim, target_dim):
-            encoder = fc_net(in_dim, 35, hidden_dims=[35])
-            classifier = torch.nn.Linear(35, datasets.y_dim)
-            return torch.nn.Sequential(encoder, classifier)
-
-        classifier_fn = adult_fc_net
-    else:
-        classifier_fn = mp_64x64_net
+    if args.method == "kamiran":
+        instance_weights = get_instance_weights(train_data)
+        train_data = IntanceWeightedDataset(train_data, instance_weights=instance_weights)
 
     train_loader = DataLoader(train_data, batch_size=args.batch_size, pin_memory=True, shuffle=True)
     test_loader = DataLoader(
@@ -63,6 +151,21 @@ def main():
     )
 
     input_shape = get_data_dim(train_loader)
+
+    #  Construct the network
+    if args.dataset == "cmnist":
+        classifier_fn = mp_32x32_net
+    elif args.dataset == "adult":
+
+        def adult_fc_net(in_dim, target_dim):
+            encoder = fc_net(in_dim, 35, hidden_dims=[35])
+            classifier = torch.nn.Linear(35, target_dim)
+            return torch.nn.Sequential(encoder, classifier)
+
+        classifier_fn = adult_fc_net
+    else:
+        classifier_fn = mp_64x64_net
+
     target_dim = datasets.s_dim if args.pred_s else datasets.y_dim
 
     classifier: Classifier = Classifier(
@@ -72,25 +175,38 @@ def main():
     )
     classifier.to(device)
 
-    classifier.fit(
-        train_loader, test_data=test_loader, epochs=args.epochs, device=device, pred_s=False
+    print(args)
+    if args.method == "kamiran":
+        train_fn = TrainKamiran()
+    else:
+        train_fn = TrainNaive()
+
+    train_fn(
+        classifier,
+        train_loader=train_loader,
+        test_loader=test_loader,
+        epochs=args.epochs,
+        device=device,
+        pred_s=False,
     )
 
     preds, ground_truths, sens = classifier.predict_dataset(test_data, device=device)
-    preds = pd.DataFrame(preds, columns=["labels"])
+    preds = Prediction(pd.DataFrame(preds, columns=["labels"]))
+
     ground_truths = DataTuple(
         x=pd.DataFrame(sens, columns=["sens"]),
         s=pd.DataFrame(sens, columns=["sens"]),
         y=pd.DataFrame(ground_truths, columns=["labels"]),
     )
 
-    full_name = f"{args.dataset}_naive_baseline"
+    full_name = f"{args.dataset}_{args.method}_baseline"
     if args.dataset == "cmnist":
         full_name += "_greyscale" if args.greyscale else "_color"
     elif args.dataset == "celeba":
         full_name += f"_{args.celeba_sens_attr}"
         full_name += f"_{args.celeba_target_attr}"
     full_name += f"_{str(args.epochs)}epochs"
+
     metrics = run_metrics(
         preds,
         ground_truths,
@@ -123,6 +239,12 @@ def main():
                 else:
                     f.write(",".join(["Mix_fact"] + [str(k) for k in metrics.keys()]) + "\n")
                 f.write(value_list + "\n")
+
+
+def main():
+    args = BaselineArgs(explicit_bool=True, underscores_to_dashes=True)
+    args.parse_args()
+    run_baseline(args=args)
 
 
 if __name__ == "__main__":
