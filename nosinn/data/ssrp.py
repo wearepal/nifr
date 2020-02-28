d675f71714081782d843ea68c26f2ffd3ca5389b
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2020-02-28 17:05:20 +0000

    sdsadsa

--- nosinn/data/ssrp.py
+++ nosinn/data/ssrp.py
@@ -14,8 +14,7 @@ def _loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
-        return img  #.convert('RGB')
-
+        return img.copy()
   
 class SSRP(Dataset):
     _FILE_ID = "1RE4srtC63VnyU0e1qx16QNdjyyQXg2hj"
