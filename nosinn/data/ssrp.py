e36de756bf0532a191619be66d6f6d853871f77e
Myles Bartlett <44702700+MylesBartlett@users.noreply.github.com>
2020-02-28 17:02:35 +0000

    Return img in two steps.

--- nosinn/data/ssrp.py
+++ nosinn/data/ssrp.py
@@ -14,7 +14,7 @@ def _loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
-        return img.convert('RGB')
+        return img  #.convert('RGB')
 
   
 class SSRP(Dataset):
