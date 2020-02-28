deb0110af6504ee395f48754e8b1307824362ede
thomkeh <7741417+thomkeh@users.noreply.github.com>
2020-02-28 14:14:50 +0000

    Add return statement to shrink_dataset

--- nosinn/data/misc.py
+++ nosinn/data/misc.py
@@ -26,7 +26,7 @@ def train_test_split(dataset: Dataset, train_pcnt: float) -> List[Subset]:
 
 
 def shrink_dataset(dataset: Dataset, pcnt: float) -> Subset:
-    train_test_split(dataset, train_pcnt=pcnt)
+    return train_test_split(dataset, train_pcnt=pcnt)
 
 
 def set_transform(dataset, transform):
