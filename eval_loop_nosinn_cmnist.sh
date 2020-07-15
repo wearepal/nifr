f6aee74cf69a044576e8d5aefc4655a9bab34665
Thomas M <thomas5max@gmail.com>
2020-07-15 18:05:41 +0100

    Update eval loop for cmnist

--- eval_loop_nosinn_cmnist.sh
+++ eval_loop_nosinn_cmnist.sh
@@ -1,1 +1,37 @@
-python start.py --train-on-recon False --level-depth 3 --levels 3 --disc-channels 512 --coupling-depth 1 --coupling-channels 256 --glow True --batch-norm False --spectral-norm False --dataset cmnist --disc-lr 3e-4 --lr 3e-4 --gamma 1 --use-comet True --nll-weight 1e-2 --base-density normal --no-scaling False --data-pcnt 1 --val-freq 3 --super-val True --scale 0.02 --eval-epochs 40 --batch-size 128 --weight-decay 1e-4 --zs-frac 0.025 --padding 2 --autoencode True --ae-epochs 5 --ae-levels 2 --ae-enc-dim 3 --results-csv aelevels2_huber.csv --epochs 100 --reshape-method squeeze --ae-loss huber --scale 0.02
+#!/bin/bash
+
+scales=( 0 0.01 0.02 0.03 0.04 0.05 )
+
+function run_cmnist() {
+	for scale in "${frac_size[@]}"; do
+		qsub -pe smpslots 6 python.job start_nosinn.py \
+		--dataset cmnist \
+		--levels 3 \
+		--level-depth 24 \
+		--glow True \
+		--reshape-method squeeze \
+		--autoencode False \
+		--input-noise True \
+		--quant-level 5 \
+		--use-wandb True \
+		--factor-splits 0=0.5 1=0.5 \
+		--train-on-recon False \
+		--recon-detach False \
+		--batch-size 256 \
+		--test-batch-size 512 \
+		--nll-weight 1 \
+		--pred-s-weight 1e-2 \
+		--zs-frac 0.002 \
+		--coupling-channels 512 \
+		--super-val True \
+		--super-val-freq 5000 \
+		--val-freq 1 \
+		--num-discs 1 \
+		--disc-channels 512 \
+		--level-depth 24 \
+		--num-discs 3 \
+		--scale $scale "$@"
+	done
+}
+
+run_cmnist --gpu 0 --results-csv more_eval.csv "$@"
