8b280f8e645980faac6d1a8511d7d4e5b55bcfdd
Thomas M <thomas5max@gmail.com>
2020-07-16 16:48:04 +0000

    Update eval_loop_naive_cmnist.sh

--- eval_loop_naive_cmnist.sh
+++ eval_loop_naive_cmnist.sh
@@ -2,13 +2,22 @@
 
 epochs="40"
 
-for greyscale in "False" "True"
-do
-for scale in "0.0" "0.01" "0.02" "0.03" "0.04" "0.05"
-do
-	python run_naive_baseline.py \
-		--dataset cmnist --scale $scale --greyscale $greyscale \
-		--lr 1e-3 --batch-size 128 --weight-decay 0 --epochs $epochs \
-		--padding 2 --pred-s False
-done
-done
+function run_baseline() {
+	for scale in "0.0" "0.01" "0.02" "0.03" "0.04" "0.05"
+	do
+		echo $scale
+		qsub -pe smpslots 3 python.job run_simple_baselines.py \
+			--dataset cmnist \
+			--lr 1e-3 \
+			--batch-size 128 \
+			--weight-decay 0 \
+			--epochs $epochs \
+			--padding 2 \
+			--pred-s False \
+			--scale $scale "$@"
+		sleep 1
+	done
+}
+
+run_baseline --greyscale False --results-csv more_metrics.csv "$@"
+run_baseline --greyscale True --results-csv greyscale_more_metrics.csv "$@"
