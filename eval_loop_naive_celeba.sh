587f034c2b1caf9514c373c313e7cfac312fd797
Thomas MK <thomas5max@gmail.com>
2023-01-06 18:51:13 +0100

    More epochs replaced

--- eval_loop_naive_celeba.sh
+++ eval_loop_naive_celeba.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-epochs="40"
+iters="100000"
 
 #for mf in "0.0" "0.1" "0.2" "0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9" "1.0"
 for mf in "0.0" "1.0"
 do
 	python run_simple_baselines.py \
 		--dataset celeba --task-mixing-factor $mf \
-		--lr 1e-3 --batch-size 128 --weight-decay 0 --epochs $epochs \
+		--lr 1e-3 --batch-size 128 --weight-decay 0 --iters $iters \
 		--pred-s False "$@"
 done
 
